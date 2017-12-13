4688835c96841089556e1f63626733dd0775c078
Justin Wojciechowski <justinwojo1@gmail.com>
2017-12-12 19:35:20 -0600

    Day 10 temp

--- Day10/Day10.cs
+++ Day10/Day10.cs
@@ -11,10 +11,27 @@ namespace Advent
     {
         public static string GetPart1Answer()
         {
-            var lines = File.ReadLines("Day10/day10input.txt");
+            var input = Array.ConvertAll(File.ReadLines("Day10/day10input.txt").First().Split(','), int.Parse);
+            var list = GenerateList(256);
+            int skipSize = 0;
+            int pointer = 0;
 
+            foreach(var lengths in input)
+            {
+                //for(int i=pointer; i<)
+            }
 
             return "";
+        }
+
+        private static List<int> GenerateList(int length)
+        {
+            var list = new List<int>();
+            for(int i=0; i<length; i++)
+            {
+                list.Add(i);
+            }
+            return list;
         }
 
         public static string GetPart2Answer()
